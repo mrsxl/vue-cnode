1472968c0a4e203c4f68ae0323991a403d55426b
sxl <sxl@sxldeMac.lan>
2017-09-08 10:35:42 +0800

    修改评论列表的样式

--- src/components/commentItem.vue
+++ src/components/commentItem.vue
@@ -1,7 +1,7 @@
 <template>
 	<a class="comment-item">
 		<div class="comment">
-			<img :src="comment.author.avatar_url"> 		
+			<!-- <img :src="comment.author.avatar_url"> 		
 			<div class="content">
 				<div class="commentItem-header">
 					<h4>{{comment.author.loginname}}</h4>
@@ -11,6 +11,18 @@
 				<div class="commentItem-footer">
 					<span><i class="iconfont icon-zan"></i>{{comment.ups.length === 0 ? '' : comment.ups.length}}</span><span>回复</span> <span>{{formateTime(comment.create_at)}}</span>
 				</div>
+			</div> -->
+			<div class="commentItem-header">
+				<img :src="comment.author.avatar_url">
+				<div class="author-info">
+					<h4>{{comment.author.loginname}}</h4>
+					<time>{{formateTime(comment.create_at)}}</time>
+				</div>
+				<div class="floor">#1</div>
+			</div>
+			<div class="commentItem-content markdown-body" v-html="comment.content"></div>
+			<div class="commentItem-footer">
+				<span><i class="iconfont icon-zan"></i>{{comment.ups.length === 0 ? '' : comment.ups.length}}</span><span>回复</span>
 			</div>
 		</div>
 	</a>
@@ -38,48 +50,59 @@
 	.comment-item {
 		display: inline-block;
 		width: 100%;
-
+		
 		.comment {
 			display: flex;
+			flex-direction: column;
 
-			img {
-	  		margin-right: 10px;
-	      width: 40px;
-	      height: 40px;
-	      border: 1px solid #ccc;
-	      border-radius: 50%;
-	  	}
-	  	.content {
-	  		flex: 1;
-				
-				.commentItem-header {
-					display: flex;
+			.commentItem-header {
+				display: flex;
+				align-items: center;
 
-					* {
-						flex: 1;
+				img {
+					margin-right: 15px;
+		      width: 40px;
+		      height: 40px;
+		      border: 1px solid #ccc;
+		      border-radius: 50%;
+				}
+				.author-info {
+					flex: 2;
+
+					h4 {
+						color: #444;
 					}
-					div{
-						text-align: right;
+					time {
+						font-size: 14px;
+						color: #969896;
 					}
 				}
-				.commentItem-content {
-					margin: 5px 0 10px;
-				}	
-				.commentItem-footer {
+				.floor {
+					flex: 1;
+					align-self: flex-start;
+					text-align: right;
+					color: #80bd01;
+				}
+			}
+			.commentItem-content {
+				margin-top: 10px;
+				color: #474747;
+			}	
+			.commentItem-footer {
+				text-align: right;
+				font-size: 14px;
+				line-height: 14px;
+				color: #969896;
+
+				span:first-child {
+					margin-right: 20px;
+				}
+
+				i {
+					margin-right: 5px;
 					font-size: 14px;
-					line-height: 14px;
-					color: #969896;
-
-					span {
-						margin-right: 15px;
-
-						i {
-							margin-right: 5px;
-							font-size: 14px;
-						}
-					}
 				}
-	  	}
+			}
 		}
 	}
 </style>
